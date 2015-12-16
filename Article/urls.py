73b7a1086c8c5b3f5cf4d03709999695f2db3c24
monokbaev <monokbaev@gmail.com>
2015-12-17 00:59:54 +0600

    Added Like counter

--- Article/urls.py
+++ Article/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^articles/(?P<article_id>\d+)/$', 'Article.views.article'),
+    url(r'^articles/addlike/(?P<article_id>\d+)/$', 'Article.views.addlike'),
     url(r'^$', 'Article.views.articles'),
 ]
--- Article/views.py
+++ Article/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, render_to_response
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.shortcuts import render, render_to_response, redirect
 from Article.models import Article, Comment, Image
 
 # Create your views here.
@@ -10,4 +12,14 @@ def articles(request):
 
 def article(request, article_id=1):
     return render_to_response('article.html', {'article': Article.objects.get(id=article_id), 'comments': Comment.objects.filter(comments_article_id=article_id)})
+
+
+def addlike(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+        article.article_likes += 1
+        article.save()
+    except ObjectDoesNotExist:
+        raise Http404
+    return redirect('/')
 
